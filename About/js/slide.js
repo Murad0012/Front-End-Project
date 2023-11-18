56d108cf54dabaf65df8b4266966f120c441c812
Predator <murad.mammedzade11@gmail.com>
2023-11-17 16:07:51 -0800

    Slider added to about page

--- About/js/slide.js
+++ About/js/slide.js
@@ -1,22 +1,12 @@
-const swiper = new Swiper('.swiper', {
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+  "--swiper-pagination-bullet-inactive-color": "#999999",
 
-    autoplay: {
-      delay: 5000,
-    },  
-    direction: 'horizontal',
-    loop: true,
-  
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-    
-  });
+
+});
